9ccfc6ffd4817dc98754f5c6d988392d6e1bf71c
Adem044 <dhiatawtawa@hotmail.com>
2024-06-27 23:38:59 +0100

    feat(UserAvatar): add link to user profile

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 import { Avatar, AvatarFallback, AvatarProps } from '@/components/ui/avatar';
 
 export default function UserAvatar(props: AvatarProps) {
     return (
-        <Avatar {...props}>
-            <AvatarFallback>AZ</AvatarFallback>
-        </Avatar>
+        <Link to="/profile">
+            <Avatar {...props}>
+                <AvatarFallback>AZ</AvatarFallback>
+            </Avatar>
+        </Link>
     );
 }
