72e85d4cda7104c6390ccbefec9d696e692430ec
Adem044 <dhiatawtawa@hotmail.com>
2024-06-27 23:29:13 +0100

    feat(logout): implement logout

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,9 +1,37 @@
+import { useNavigate } from 'react-router-dom';
+import { LogOutIcon } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+
 import NavLinks from './NavLinks';
 
 export default function Sidebar() {
     return (
         <aside className="hidden max-w-max border-r sm:block">
             <NavLinks />
+            <Logout />
         </aside>
+    );
+}
+
+function Logout() {
+    const navigate = useNavigate();
+    const logoutUser = () => {
+        localStorage.clear();
+        navigate('/sign-in');
+    };
+
+    return (
+        <div className="px-4">
+            <Button
+                className="w-full justify-start gap-4 px-4 text-destructive"
+                size="lg"
+                variant="ghost"
+                onClick={logoutUser}
+            >
+                <LogOutIcon />
+                <span>Logout</span>
+            </Button>
+        </div>
     );
 }
