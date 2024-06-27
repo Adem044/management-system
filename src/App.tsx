92a222837acd3626c52c97e0e9aa52d24bd18c02
Adem044 <dhiatawtawa@hotmail.com>
2024-06-27 22:54:29 +0100

    feat: setup authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, Navigate } from 'react-router-dom';
 
 import TopBar from '@/components/layout/TopBar';
 import Sidebar from '@/components/layout/Sidebar';
 
 export default function App() {
+    const { pathname } = useLocation();
+    const user = localStorage.getItem('user');
+
+    if (!user) {
+        return <Navigate to="/sign-in" />;
+    }
+
+    if (pathname === '/') {
+        return <Navigate to="/dashboard" />;
+    }
+
     return (
         <div className="flex min-h-screen flex-col pb-10 pt-20 sm:p-0">
             <TopBar />
--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
@@ -32,6 +33,7 @@ const formSchema = z.object({
 type TFormSchema = z.infer<typeof formSchema>;
 
 function LoginForm() {
+    const navigate = useNavigate();
     const form = useForm<TFormSchema>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -42,7 +44,11 @@ function LoginForm() {
     });
 
     async function onSubmit(values: TFormSchema) {
-        console.log(values);
+        localStorage.setItem(
+            'user',
+            JSON.stringify({ role: 'user', email: values.email }),
+        );
+        navigate('/dashboard');
     }
     return (
         <Form {...form}>
