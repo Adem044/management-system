290d90e786ed6c83e5d5c900266970142deded23
Adem044 <dhiatawtawa@hotmail.com>
2024-06-28 09:40:52 +0100

    style(AuthLayout): improve responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -40,8 +40,8 @@ export function AuthLayout() {
         return <Navigate to="/dashboard" />;
     }
     return (
-        <div className="items-center sm:grid sm:grid-cols-2">
-            <div className="container space-y-6 py-8 sm:space-y-12">
+        <div className="flex min-h-screen">
+            <div className="container flex max-w-xl grow flex-col justify-center gap-6 py-8 sm:gap-12">
                 <Outlet />
             </div>
             <WelcomeBanner />
@@ -51,7 +51,7 @@ export function AuthLayout() {
 
 function WelcomeBanner() {
     return (
-        <div className="hidden bg-[#56ccf2] pt-8 text-center text-white sm:block">
+        <div className="relative hidden max-w-md bg-[#56ccf2] pt-8 text-center text-white sm:block">
             <div className="container space-y-6">
                 <h2 className="text-2xl font-medium">
                     We give the best experience 😉
@@ -73,7 +73,11 @@ function WelcomeBanner() {
                     alt="What people say about as"
                 />
             </div>
-            <img className="-mt-80" src="/doctors.png" alt="Our doctors" />
+            <img
+                className="absolute bottom-0"
+                src="/doctors.png"
+                alt="Our doctors"
+            />
         </div>
     );
 }
