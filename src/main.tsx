afc614d7b2ec99f66163400cdaa0f9ddb8e4f5b1
Adem044 <dhiatawtawa@hotmail.com>
2024-06-29 12:33:43 +0100

    feat: setup firebase

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { initializeApp } from 'firebase/app';
 
 import './index.css';
 
@@ -21,6 +22,17 @@ import MessageDetails from '@/pages/messages/id';
 import Appointments from '@/pages/appointment';
 
 import { Toaster } from '@/components/ui/toaster';
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
 
 const router = createBrowserRouter([
     {
